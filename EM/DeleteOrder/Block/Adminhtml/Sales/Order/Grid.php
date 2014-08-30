0df7424f6dbc77469a22c1a7c2daa37c9140ccdb
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2014-08-30 13:22:13 +0200

    reformat code (no code changes);

--- EM/DeleteOrder/Block/Adminhtml/Sales/Order/Grid.php
+++ EM/DeleteOrder/Block/Adminhtml/Sales/Order/Grid.php
@@ -1,16 +1,14 @@
 <?php
-class EM_DeleteOrder_Block_Adminhtml_Sales_Order_Grid extends Mage_Adminhtml_Block_Sales_Order_Grid
-{
-    /**
-    * Adding Delete Order Mass Action
-    */
-    protected function _prepareMassaction()
-    {
-        $retValue = parent::_prepareMassaction();
-		$this->getMassactionBlock()->addItem('delete_order', array(
-             'label'=> Mage::helper('sales')->__('Delete order'),
-             'url'  => $this->getUrl('*/sales_order/deleteorder'),
-        ));
-        return $retValue;
-    }
+class EM_DeleteOrder_Block_Adminhtml_Sales_Order_Grid extends Mage_Adminhtml_Block_Sales_Order_Grid {
+  /**
+  * Adding Delete Order Mass Action
+  */
+  protected function _prepareMassaction() {
+    $retValue = parent::_prepareMassaction();
+    $this->getMassactionBlock()->addItem('delete_order', array(
+      'label'=> Mage::helper('sales')->__('Delete order'),
+      'url'  => $this->getUrl('*/sales_order/deleteorder'),
+    ));
+    return $retValue;
+  }
 }
